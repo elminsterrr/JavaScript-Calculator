20174b53d414d20f77a93cd4a401aa157ccd2ac1
elminsterrr <elminsterthewhite@gmail.com>
2017-08-02 16:20:56 +0200

    Set starting variable value to 'null'

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 // JavaScript Calculator v.2.0
 // Created by: Elminster White - elminsterthewhite@gmail.com
 
-"use strict";
+'use strict';
 
 function Calculator(calcId) {
-  const oneCalc = document.querySelector("#" + calcId);
+  const oneCalc = document.querySelector('#' + calcId);
 
   // One global variable for one CALC
   const CALC = {};
   CALC.inputs = [];
-  CALC.lastResult = undefined;
-  CALC.operationsUpdate = oneCalc.querySelector(".inputTextboxUpper");
-  CALC.screenUpdate = oneCalc.querySelector(".inputTextboxLower");
-  CALC.buttons = oneCalc.querySelectorAll("[data-value]");
+  CALC.lastResult = null;
+  CALC.operationsUpdate = oneCalc.querySelector('.inputTextboxUpper');
+  CALC.screenUpdate = oneCalc.querySelector('.inputTextboxLower');
+  CALC.buttons = oneCalc.querySelectorAll('[data-value]');
 
   // Handle CALC buttons
   CALC.buttons.forEach(button => button.addEventListener('click', whenClicked));
@@ -23,10 +23,10 @@ function Calculator(calcId) {
     const last = CALC.inputs[CALC.inputs.length - 1];
     // Function that updates inputs on screen
     function update() {
-      CALC.operationsUpdate.value = CALC.inputs.join("");
+      CALC.operationsUpdate.value = CALC.inputs.join('');
       // To avoid undefined when using CE before first getTotal
-      if (CALC.lastResult === undefined) {
-        CALC.screenUpdate.value = "...";
+      if (CALC.lastResult === null) {
+        CALC.screenUpdate.value = '...';
       } else {
         CALC.screenUpdate.value = CALC.lastResult;
       }
@@ -45,40 +45,40 @@ function Calculator(calcId) {
     // Function that calculates total value using calcBrain function
     function getTotal() {
       // Refresh is the value of last calculation
-      const refresh = calcBrain(CALC.inputs.join(""));
-      CALC.operationsUpdate.value = CALC.inputs.join("");
+      const refresh = calcBrain(CALC.inputs.join(''));
+      CALC.operationsUpdate.value = CALC.inputs.join('');
       CALC.lastResult = refresh;
       CALC.screenUpdate.value = refresh;
     }
     // Preventing this 1++2 from happening
-    if ((click === "+" || click === "-" || click === "*" || click === "/" ||
-        click === ".") && (last === "+" || last === "-" || last === "*" ||
-        last === "/" || last === ".")) {
+    if ((click === '+' || click === '-' || click === '*' || click === '/' ||
+        click === '.') && (last === '+' || last === '-' || last === '*' ||
+        last === '/' || last === '.')) {
       return; // Do nothing when this conditions are fulfilled
     }
     // Handling special buttons
-    if (click === "deleteAll") {
+    if (click === 'deleteAll') {
       // All Clear
       CALC.inputs = [];
-      CALC.lastResult = undefined;
-      CALC.operationsUpdate.value = "";
+      CALC.lastResult = null;
+      CALC.operationsUpdate.value = '';
       CALC.screenUpdate.value = 0;
-    } else if (click === "backOne") {
+    } else if (click === 'backOne') {
       // Clear Entry
-      // Only when we have something in "inputs" array we can use pop
+      // Only when we have something in 'inputs' array we can use pop
       if (CALC.inputs.length > 0 && CALC.inputs.length === 1) {
-        // If CALC.inputs.length is === 1 "Clear Entry" behaves just like "AC"
+        // If CALC.inputs.length is === 1 'Clear Entry' behaves just like 'AC'
         CALC.inputs = [];
-        CALC.lastResult = undefined;
-        CALC.operationsUpdate.value = "";
+        CALC.lastResult = null;
+        CALC.operationsUpdate.value = '';
         CALC.screenUpdate.value = 0;
       } else if (CALC.inputs.length > 0 && CALC.inputs.length !== 1) {
         CALC.inputs.pop();
         update();
       }
-    } else if (click === "total") {
+    } else if (click === 'total') {
       // When there is nothing to calculate do nothing
-      if ((CALC.inputs.length === 0) && (CALC.lastResult === undefined)) {
+      if ((CALC.inputs.length === 0) && (CALC.lastResult === null)) {
         return; // Do nothing when this conditions are fulfilled
       }
       // Calculating total value
@@ -100,6 +100,6 @@ function Calculator(calcId) {
     */
   }
 }
-const calculator1 = new Calculator("calculator1");
-const calculator2 = new Calculator("calculator2");
-const calculator3 = new Calculator("calculator3");
+const calculator1 = new Calculator('calculator1');
+const calculator2 = new Calculator('calculator2');
+const calculator3 = new Calculator('calculator3');
